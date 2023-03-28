264241b8c3a4a99577e8783e75ab440b470058f5
Bernardo-MG <programming@bernardomg.com>
2023-03-28 12:37:23 +0200

    Cleaned up code

--- src/main/java/com/bernardomg/example/spring/execjar/Main.java
+++ src/main/java/com/bernardomg/example/spring/execjar/Main.java
@@ -1,22 +1,34 @@
 
 package com.bernardomg.example.spring.execjar;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
+/**
+ * Main entry. This is what makes the project executable.
+ * <p>
+ * All it does is printing to the log.
+ *
+ * @author Bernardo Mart&iacute;nez Garrido
+ *
+ */
+@Slf4j
 public class Main {
 
     /**
-     * Logger.
+     * Main method. This will be executed when running th project.
+     *
+     * @param args
+     *            execution arguments
      */
-    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
-
-    public Main() {
-        super();
+    public static void main(final String[] args) {
+        log.info("Executed correctly");
     }
 
-    public static void main(final String[] args) {
-        LOGGER.info("Executed correctly");
+    /**
+     * Default constructor.
+     */
+    private Main() {
+        super();
     }
 
 }
