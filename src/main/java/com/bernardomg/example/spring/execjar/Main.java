ab21d5c7a77a3cce019dbbaba1a7c1fc90d706c8
Bernardo-MG <programming@bernardomg.com>
2025-07-08 00:16:14 +0200

    Removed lombok

--- src/main/java/com/bernardomg/example/spring/execjar/Main.java
+++ src/main/java/com/bernardomg/example/spring/execjar/Main.java
@@ -1,7 +1,8 @@
 
 package com.bernardomg.example.spring.execjar;
 
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Main entry. This is what makes the project executable.
@@ -11,8 +12,12 @@ import lombok.extern.slf4j.Slf4j;
  * @author Bernardo Mart&iacute;nez Garrido
  *
  */
-@Slf4j
 public class Main {
+
+    /**
+     * Logger for the class.
+     */
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
 
     /**
      * Main method. This will be executed when running th project.
